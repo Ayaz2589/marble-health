2c1e80ec23b7a057ce1015e10b5eeca3184cbf60
Ayaz Uddin <ayaz@crossterra.com>
2025-03-13 10:34:47 -0400

    add a basic calender

--- src/app/calender/page.tsx
+++ src/app/calender/page.tsx
@@ -1,8 +1,5 @@
+import { CalendarView } from "@/components";
+
 export default function CalendarPage() {
-  return (
-    <main style={{ padding: "2rem" }}>
-      <h1>Calendar Page</h1>
-      <p>This is a basic page for our scheduling calendar.</p>
-    </main>
-  );
+  return <CalendarView />;
 }
